3c8dbd725216b5784c74e2c37dcd707f6622b6a2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-03-20 15:30:06 +0100

    Initial implementation

---
+++ DiGi.User/DiGi.User/Classes/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace DiGi.User.Classes
+{
+    public class User : Core.Classes.SerializableObject
+    {
+        public User(string? email)
+            : base()
+        {
+            Email = email;
+        }
+
+        public User(User user)
+            : base(user)
+        {
+            if (user is not null)
+            {
+                Email = user.Email;
+                LastName = user.LastName;
+                FirstName = user.FirstName;
+            }
+        }
+
+        public User(JsonObject jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+
+        [JsonInclude, JsonPropertyName("Email")]
+        public string? Email { get; set; }
+
+        [JsonInclude, JsonPropertyName("FirstName")]
+        public string? FirstName { get; set; }
+
+        [JsonInclude, JsonPropertyName("LastName")]
+        public string? LastName { get; set; }
+    }
+}
---
+++ DiGi.User/DiGi.User/Classes/UserLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace DiGi.User.Classes
+{
+    public class UserLogin : Core.Classes.SerializableObject
+    {
+        public UserLogin(string? email)
+            : base()
+        {
+            Email = email;
+        }
+
+        public UserLogin(UserLogin userLogin)
+            : base(userLogin)
+        {
+            if (userLogin is not null)
+            {
+                Email = userLogin.Email;
+                Password = userLogin.Password;
+            }
+        }
+
+        public UserLogin(JsonObject jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+
+        [JsonInclude, JsonPropertyName("Email")]
+        public string? Email { get; private set; }
+
+        [JsonInclude, JsonPropertyName("Password")]
+        public string? Password { get; private set; }
+
+    }
+}
