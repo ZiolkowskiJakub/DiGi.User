465448bab12b0d216e4e7d67e615713c456f22ec
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-05 09:50:59 +0200

    Documentation added

--- DiGi.User/Classes/User.cs
+++ DiGi.User/Classes/User.cs
@@ -5,12 +5,20 @@ namespace DiGi.User.Classes
 {
     public class User : Core.Classes.SerializableObject
     {
+        /// <summary>
+        /// Initializes a new instance of the User class with a specified email.
+        /// </summary>
+        /// <param name="email">The email address of the user.</param>
         public User(string? email)
             : base()
         {
             Email = email;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the User class by copying another User object.
+        /// </summary>
+        /// <param name="user">The source User object to copy from.</param>
         public User(User user)
             : base(user)
         {
@@ -22,18 +30,31 @@ namespace DiGi.User.Classes
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the User class from a JsonObject.
+        /// </summary>
+        /// <param name="jsonObject">The JSON object containing user data.</param>
         public User(JsonObject jsonObject)
             : base(jsonObject)
         {
 
         }
 
+        /// <summary>
+        /// Gets or sets the email address of the user.
+        /// </summary>
         [JsonInclude, JsonPropertyName("Email")]
         public string? Email { get; set; }
 
+        /// <summary>
+        /// Gets or sets the first name of the user.
+        /// </summary>
         [JsonInclude, JsonPropertyName("FirstName")]
         public string? FirstName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the last name of the user.
+        /// </summary>
         [JsonInclude, JsonPropertyName("LastName")]
         public string? LastName { get; set; }
     }
--- DiGi.User/Classes/UserLogin.cs
+++ DiGi.User/Classes/UserLogin.cs
@@ -6,12 +6,20 @@ namespace DiGi.User.Classes
 {
     public class UserLogin : Core.Classes.SerializableObject
     {
+        /// <summary>
+        /// Initializes a new instance of the UserLogin class with the specified email.
+        /// </summary>
+        /// <param name="email">The email address of the user.</param>
         public UserLogin(string? email)
             : base()
         {
             Email = email;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the UserLogin class by copying another UserLogin object.
+        /// </summary>
+        /// <param name="userLogin">The source UserLogin object to copy from.</param>
         public UserLogin(UserLogin userLogin)
             : base(userLogin)
         {
@@ -22,15 +30,25 @@ namespace DiGi.User.Classes
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the UserLogin class from a JsonObject.
+        /// </summary>
+        /// <param name="jsonObject">The JSON object containing user login data.</param>
         public UserLogin(JsonObject jsonObject)
             : base(jsonObject)
         {
 
         }
 
+        /// <summary>
+        /// Gets the email address of the user.
+        /// </summary>
         [JsonInclude, JsonPropertyName("Email")]
         public string? Email { get; private set; }
 
+        /// <summary>
+        /// Gets the password of the user.
+        /// </summary>
         [JsonInclude, JsonPropertyName("Password")]
         public string? Password { get; private set; }
 
